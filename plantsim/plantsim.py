5966be1302c2320e57e7f12f46405d489b471bee
lortane <lortane@pm.me>
2024-09-17 07:51:19 +0200

    CHORE: add prints and comments

--- plantsim/plantsim.py
+++ plantsim/plantsim.py
@@ -8,21 +8,25 @@ import os
 import win32com.client as win32
 
 from .error import Error
-from .attribute_explorer import AttributeExplorer
 from .exception import *
 from .table import DataFrame
 
 class Plantsim:
 
-    def __init__(self, version='', visible=True, trust_models=False, license_type='Professional'):
-        print(f'\n+++ Launching Plant Simulation (version={version}, license={license_type}, visibility={visible}, trust_models={trust_models}) +++\n')
+    def __init__(self, version='', visible=True, trust_models=False, license_type='Professional'):        
+        """
+        Initialize the Plant Simulation application
+        :param version: The version of Plant Simulation to use
+        :param visible: If True, the Plant Simulation window will be visible
+        :param trust_models: If True, Plant Simulation models will be allowed to access the computer
+        :param license_type: The license type to use (Professional, Student, Viewer)
+        """
+
+        print(f":: Launching Plant Simulation (version={version}, license={license_type}, visibility={visible}, trust_models={trust_models})")
 
         dispatch_string = 'Tecnomatix.PlantSimulation.RemoteControl'
         if version != '':
             dispatch_string += f'.{version}'
-
-        # Late-binding version
-        #self.plantsim = win32.Dispatch(dispatch_string)
 
         # Early-binding version
         try:
@@ -49,36 +53,59 @@ class Plantsim:
         self.event_controller = ''
 
     def load_model(self, filepath):
+        """
+        Load a Plant Simulation model
+        :param filepath: The path to the model file
+        """
         
         if not os.path.exists(filepath):
             raise FileNotFoundError(f'Model file "{filepath}" not found!')
 
-        print(f'Loading model "{filepath}"...')
+        print(f'  Loading model "{filepath}"...')
         try:
             self.plantsim.LoadModel(filepath)
         except BaseException as e:
             if Error.extract(e.args) == Error.Code.INVALID_LICENSE:
                 raise InvalidLicenseError(self.license_type)
+            
+        print(f'  Model "{filepath}" loaded.')
 
     def set_path_context(self, path_context):
+        """
+        Set the path context to the current path context
+        :param path_context: The path context in PlantSim
+        """
 
         self.path_context = path_context
         self.plantsim.SetPathContext(self.path_context)
 
+        print(f'  Path context set to "{self.path_context}".')
+
     def set_event_controller(self, event_controller='EventController'):
+        """
+        Set the event controller to the current path context
+        :param event_controller: The name of the event controller in PlantSim
+        """
 
         if not self.path_context:
             raise CommandOrderError("set_event_controller", "set_path_context")
         self.event_controller = f'{self.path_context}.{event_controller}'
 
+        print(f'  Event controller set to "{self.event_controller}".')
+
     def reset_simulation(self):
+        """
+        Reset the simulation
+        """
 
         if not self.event_controller:
             raise CommandOrderError("reset_simulation", "set_event_controller")
-
         self.plantsim.ResetSimulation(self.event_controller)
 
     def start_simulation(self):
+        """
+        Start the simulation
+        """
 
         if not self.event_controller:
             raise CommandOrderError("start_simulation", "set_event_controller")
@@ -101,10 +128,20 @@ class Plantsim:
         return self.plantsim.IsSimulationRunning()
 
     def get_value(self, object_name):
+        """
+        Get the value of an object 
+        :param object_name: The name of the object in PlantSim
+        :return: The value of the object
+        """
 
         return self.plantsim.GetValue(object_name)
 
     def set_value(self, object_name, value):
+        """
+        Set the value of an object in PlantSim
+        :param object_name: The name of the object in PlantSim
+        :param value: The value to set
+        """
 
         self.plantsim.SetValue(object_name, value)
 
@@ -152,5 +189,8 @@ class Plantsim:
                 self.set_value(f'{table_name}[{col_idx + 1}, {row_idx + 1}]', value)
 
     def quit(self):
+        """
+        Quits the Plant Simulation application
+        """
 
         self.plantsim.Quit()
