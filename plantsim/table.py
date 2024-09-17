62d04e03cb541484cc5bae4807f62b4b2b598b68
lortane <lortane@pm.me>
2024-09-17 08:01:41 +0200

    CHORE: update copyright in files

--- plantsim/table.py
+++ plantsim/table.py
@@ -1,5 +1,6 @@
 """
 Copyright (c) 2021 Tilo van Ekeris / TMDT, University of Wuppertal
+Copyright (c) 2024 Lorenzo Ortiz Aneiros
 Distributed under the MIT license, see the accompanying
 file LICENSE or https://opensource.org/licenses/MIT
 """
