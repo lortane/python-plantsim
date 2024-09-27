d7aec9c5600f2d091c7e54a64bb09553f5a06251
lortane <lortane@pm.me>
2024-09-27 11:00:33 +0200

    CHORE: bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="plantsim",
-    version="0.1.1",
+    version="0.2.0",
     author="Tilo van Ekeris, Lorenzo Ortiz Aneiros",
     author_email="tilo@vanekeris.de, lortane@pm.me",
     description="Python wrapper for Plant Simulation",
