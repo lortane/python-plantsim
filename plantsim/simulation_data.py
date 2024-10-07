c447b867bea8b944d87fcd52cc4f011cbbd2a115
lortane <lortane@pm.me>
2024-10-07 12:10:46 +0200

    Feat: add __str__ and iterator

--- plantsim/simulation_data.py
+++ plantsim/simulation_data.py
@@ -29,6 +29,15 @@ class SimulationData:
         else:
             raise StopIteration()
 
+    def __str__(self) -> str:
+        return (
+            f"SimulationData(\n"
+            f"  input_variables={self._input_variables},\n"
+            f"  values={self._values},\n"
+            f"  output_variables={self._output_variables}\n"
+            f")"
+        )
+
     def get_output_variables(self) -> List[str]:
         return self._output_variables
 
@@ -37,3 +46,23 @@ class SimulationResult:
     def __init__(self, output_variables: List[str], values: List[Any]):
         self._output_variables = output_variables
         self._values = values
+
+    def __iter__(self) -> Iterator[Tuple[str, Any]]:
+        self._index = 0
+        return self
+
+    def __next__(self) -> Tuple[str, Any]:
+        if self._index < len(self._output_variables):
+            result = (self._output_variables[self._index], self._values[self._index])
+            self._index += 1
+            return result
+        else:
+            raise StopIteration()
+
+    def __str__(self) -> str:
+        return (
+            f"SimulationResult(\n"
+            f"  output_variables={self._output_variables},\n"
+            f"  values={self._values}\n"
+            f")"
+        )
